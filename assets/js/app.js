cc87299ce59476aed10099117cf63089b38ab528
azizsige <kurniawan00azizsige@gmail.com>
2022-11-03 12:07:01 +0700

    update: make responsive in navbar right top

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,29 @@
 const contentBtn = document.getElementById("contentBtn");
 const hamburger = document.getElementById("hamburger");
-
+let width = window.innerWidth;
 hamburger.addEventListener("click", function () {
   contentBtn.classList.toggle("show");
   contentBtn.classList.toggle("hidden");
 });
+
+window.addEventListener("resize", function () {
+  width = window.innerWidth;
+  // console.log(width);
+  if (width < 1024) {
+    // console.log("width > 768");
+    contentBtn.classList.remove("show");
+    contentBtn.classList.add("hidden");
+  } else {
+    contentBtn.classList.add("show");
+    contentBtn.classList.remove("hidden");
+  }
+});
+
+if (width < 1024) {
+  // console.log("width > 768");
+  contentBtn.classList.remove("show");
+  contentBtn.classList.add("hidden");
+} else {
+  contentBtn.classList.add("show");
+  contentBtn.classList.remove("hidden");
+}
