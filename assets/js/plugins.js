703480ddf883d6433b2096acb34e11de5d56b694
azizsige <kurniawan00azizsige@gmail.com>
2022-11-07 13:35:18 +0700

    update: change trigger tooltip

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -2,11 +2,15 @@ var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
+  return new bootstrap.Tooltip(tooltipTriggerEl, {
+    trigger: "hover",
+  });
 });
 var tooltipTriggerListTwo = [].slice.call(
   document.querySelectorAll('[data-tooltip="tooltip"]')
 );
 var tooltipList = tooltipTriggerListTwo.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
+  return new bootstrap.Tooltip(tooltipTriggerEl, {
+    trigger: "hover",
+  });
 });
